bc43412796d3d0680d2569d91f4e45eb332cd361
Erno Pakarinen <codesmith.fi@gmail.com>
2021-12-11 18:46:28 +0200

    Added visual studio (2022) project files. Bug fixes

--- NameGenerator.h
+++ NameGenerator.h
@@ -9,7 +9,25 @@ namespace codesmith
         "lee", "ni", "el", "ram", "rond"
         "le", "go", "las", "gui", "ne", "ve", "re"
         "ar", "nold", "ara", "gorn", "con",
-        "lou", "hi", "ha", "ho", "zap", "the", "ma", "gick"
+        "lou", "hi", "ha", "ho", "zap", "the", "ma", "gick",
+        "jas", "min", "on", "bal", "ta", "zar",
+        "men", "thol", "su", "vi", "er", "no",
+        "spar", "hawk", "ke", "mi", "ber", "he",
+        "dos", "sal", "adir", "na", "tash", "ha",
+        "inka", "cae", "sar", "sha", "tho",
+        "bel", "gar", "ion", "ath", "pol", "gara",
+        "sau", "ron", "gand", "alf", "bil", "bo",
+        "a", "am", "ain", "aro", "anx", "ax", "ayx",
+        "boo", "blah", "benz", "in", "gaz", "olr", "ne",
+        "cha", "too", "min", "olth", "rez", "read", "me", "elp",
+        "ix", "moron", "utns", "alm", "elm", "ilm", "dona", "dina",
+        "six", "in", "nid", "nim", "hop", "melz", "janz", "as",
+        "yrn", "asm", "min", "tho", "mas", "moon", "lite", "ern", "otu",
+        "omain", "en", "sal", "adir", "leg", "end", "dom", "ains",
+        "cux", "bet", "ter", "ton", "not", "to", "tak", "mut",
+        "de", "dip", "wat", "rand", "xo", "mel", "pid",
+        "eiz", "ex", "epat", "ena", "ble", "fuc", "byt", "me",
+        "ux", "ex", "pox", "xep", "soc", "ra", "tes", "uni", "hop"
     };
 
     class Names
@@ -30,7 +48,7 @@ namespace codesmith
 
         const std::string& firstName(int aMaxParts=4) {
             // the first name has always atleast one syllable
-            auto  c=m_RandomGen.randomInt(1, aMaxParts);
+            auto  c=m_RandomGen.randomInt(1, aMaxParts+1);
             m_prevFirstName.clear();
             m_prevFirstName = makeNameString(c);
             return m_prevFirstName;
